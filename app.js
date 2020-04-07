42365d82d87333470212b43b3ea45819c6f0a80a
Gianmarcos <gians96@gmail.com>
2020-04-06 23:37:09 -0500

    leergit.md diccionario de git22

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
 app.get('/index', (req, res) => {
-  res.send('Hello Woasdasrld!');
+  res.send('Hello Woasdasdasrld!');
 });
   
 
